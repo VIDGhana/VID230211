f27e5f69af5fbc82763020295ee7b6003110893d
PAUL OFFEI (GODINME) <poffei@st.ug.edu.gh>
2023-02-21 07:20:14 +0000

    added practice and lab work for week one

---
+++ wk_one/first_lab.py
@@ -0,0 +1,35 @@
+# print output
+# This is my comment
+
+"""
+print("Hello World")
+
+# user input
+name = input("What is your name? ")
+print("Hello " + name)
+print("Hello ", name)
+print(f"Hello {name}")
+
+age = input("How old are you? ")
+print("You are " + age + " years old.")
+print(f"You are {age} years old.")
+"""
+
+# Value Error
+# float("hello")
+print(float(3))
+print(int(4.5))
+
+
+# Type Error
+15 - 10
+# 15 - hi
+
+# Name Error
+y = 10
+x = y
+
+
+# Syntax Error
+#1 = 1
+1 == 1
---
+++ wk_one/second_lab.py
@@ -0,0 +1,17 @@
+nana = 4
+# 4 = corgis
+
+# 4corgis = 4
+
+prince_4 = 7
+
+number_of_corgis = 4
+numberOfCorgis = 4
+
+forProfessorParlett = "Hello"
+
+x = 1
+# x + 1 = 3
+x = 3 + 1 * 4
+
+print(nana)
